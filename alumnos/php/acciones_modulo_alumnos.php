22cc1d2ede9a1b24b3363ac956b181719dca73e6
royercross <rogelio_noris@Unknown-2.local>
2014-05-08 16:25:32 -0600

    Actualizado Acciones Modulo Alumnos

--- alumnos/php/acciones_modulo_alumnos.php
+++ alumnos/php/acciones_modulo_alumnos.php
@@ -1,10 +1,7 @@
 <?php
 @include("../../check.php");
-
-if(isset($_POST['txtNombre']) && isset($_POST['txtApellidoPaterno']) && isset($_POST['txtApellidoMaterno']) && isset($_POST['accion'])){
-	
-	$accion=$_POST['accion'];
-
+if(isset($_POST['accion'])){
+	$accion=$_POST['accion'];	
 	$id_usuario=$_SESSION['id_usuario'];
 	$id_facultad=$_SESSION['id_facultad'];
 
@@ -27,16 +24,19 @@ if(isset($_POST['txtNombre']) && isset($_POST['txtApellidoPaterno']) && isset($_
 	
 	
 	if($accion=='agrega'){
-		$query="INSERT INTO alumnos(generacion,nombre,apellido_paterno,apellido_materno,sexo,email,telefono_casa,telefono_celular,domicilio,colonia,id_carrera,id_facultad) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
-		$mysql->execute($query,array($generacion,$nombre,$apellido_paterno,$apellido_materno,$sexo,$email,$telefono_casa,$telefono_celular,$domicilio,$colonia,$id_carrera,$id_facultad));				
-		$last_id=$mysql->getlastInsertedId();
-		$query="INSERT INTO c_activos (id_alumno,rkey) VALUES (?,?)";
-		$mysql->execute($query,array($last_id,md5($last_id.date())));
-		$_SESSION['mensaje']="El alumno ha sido agregado correctamente.";	
-		header('Location: modulo_alumnos.php');	
-		exit;
+		if(isset($_POST['txtNombre']) && isset($_POST['txtApellidoPaterno']) && isset($_POST['txtApellidoMaterno'])){
+			$query="INSERT INTO alumnos(generacion,nombre,apellido_paterno,apellido_materno,sexo,email,telefono_casa,telefono_celular,domicilio,colonia,id_carrera,id_facultad) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
+			print_r (array($generacion,$nombre,$apellido_paterno,$apellido_materno,$sexo,$email,$telefono_casa,$telefono_celular,$domicilio,$colonia,$id_carrera,$id_facultad));
+			$mysql->execute($query,array($generacion,$nombre,$apellido_paterno,$apellido_materno,$sexo,$email,$telefono_casa,$telefono_celular,$domicilio,$colonia,$id_carrera,$id_facultad));				
+			$last_id=$mysql->getlastInsertedId();
+			$query="INSERT INTO c_activos (id_alumno,rkey) VALUES (?,?)";
+			$mysql->execute($query,array($last_id,md5($last_id.date())));
+			$_SESSION['mensaje']="El alumno ha sido agregado correctamente.";	
+		}
+		//header('Location: modulo_alumnos.php');	
+		//exit;
 	}else
-	if($accion='modifica'){
+	if($accion=='modifica'){
 		/*
 		$id=$_POST['id'];
 		$query="UPDATE cc_actividades_extracurriculares SET nombre_actividad=?, creditos=? WHERE id_actividad_extracurricular=?";
@@ -44,8 +44,17 @@ if(isset($_POST['txtNombre']) && isset($_POST['txtApellidoPaterno']) && isset($_
 		$_SESSION['mensaje']="La actividad extracurricular ha sido actualizada correctamente.";	
 		header('Location: modulo_alumnos.php');	
 		*/
+	}else
+	if($accion=='eliminar'){		
+		$id=$_POST['id_alumno'];
+		$query="UPDATE alumnos SET status=2,fecha_modificacion=NOW(),id_usuario_modificacion=? WHERE id_alumno=?";
+		//echo $query;
+		$mysql->execute($query,array($id_usuario,$id));
+		$_SESSION['mensaje']="El alumno se ha borrado satisfactoriamente.";	
+		//header('Location: modulo_historial_creditos.php');						
 	}
-	exit;
+
+	//exit;
 }
 
 ?>
